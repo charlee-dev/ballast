aa71943a206dc1ce68aab9af6bae374b7da816cb
Adrian Witaszak <adrianwitaszak@gmail.com>
2023-09-23 11:06:31 +0100

    chore: added notebooks

---
+++ examples/compose_sharedui_kmm/shared/src/commonMain/kotlin/com/ballast/sharedui/AnyClassSharedCommon.kt
@@ -0,0 +1,11 @@
+package com.ballast.sharedui
+
+class AnyClassSharedCommon {
+    fun hello() {
+        println("Hello from AnyClassSharedCommon")
+    }
+}
+
+fun anyHello() {
+    println("Hello from anyHello")
+}
